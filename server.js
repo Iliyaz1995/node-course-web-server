74b6535ae0b0bd65d1230b5c89febd358ec650f6
Iliyaz <eliyas.sd143@gmail.com>
2017-08-25 02:32:20 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next)=>{
+  var time = new Date().toString();
+  var log = `${time}: ${req.method}${req.url}`;
+  console.log(log);
+  fs.appendFile('serverlog.log', log +'\n',(err)=>{
+    if (err) {
+console.log('unable to connect to server');
+    }
+  }
+  )
+  next();
+})
+hbs.registerHelper('currentYear',()=>{
+  return new Date().getFullYear();
+})
+
+app.get('/', (req, res) =>{
+  res.render('home.hbs',{
+    pageName: 'this is home page'
+  })
+});
+
+app.get('/help', (req, res) =>{
+  res.render('help.hbs',{
+    pageName: 'this is help page'
+  })
+});
+
+app.get('/about', (req, res) =>{
+  res.render('about.hbs',{
+    pageName: 'this is about page'
+  })
+});
+
+app.listen(3000);
